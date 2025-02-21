c15e7c039278d37758cc5a147e370ac80ebe870e
SHEMNDARONGUGI <ndaroshem@gmail.com>
2025-02-21 12:57:02 +0300

    Initial commit

---
+++ Login_Page/js/index.js
@@ -0,0 +1,8 @@
+// Saving reference for each element by getting them by id
+
+const form = document.getElementById('form');
+const full_name = document.getElementById('name');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
