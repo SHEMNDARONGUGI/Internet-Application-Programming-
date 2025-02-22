6269f1fe6f337037814cb4834056281a47511366
SHEMNDARONGUGI <ndaroshem@gmail.com>
2025-02-22 23:52:56 +0300

    Initial commit

--- Login_Page/index.php
+++ Login_Page/index.php
@@ -1,13 +1,25 @@
 <?php
+require 'config.php';
 if(isset($_POST["submit"])){
     $name = $_POST["name"];
     $username = $_POST["username"];
     $email = $_POST["email"];
     $password = $_POST["password"];
-    $password_confirm = $_POST[""];
-    $duplicate = mysqli_query($cons, "SELECT * FROM "); //where i've left from
+    $password_confirm = $_POST["password_confirm"];
+    $duplicate = mysqli_query($cons, "SELECT * FROM login WHERE username ='$username' OR email='$email'");
+    if(mysqli_num_rows($duplicate) > 0){
+        echo"
+        <script> alert ('Username or email has already taken')</script> ";
 }
-
+else{
+    if ($password == $password_confirm){
+    $query = "INSERT INTO login VALUES('','$name','$username','$email','$password')";
+    mysqli_query($conn,$query);
+    echo
+    <script> alert ('Registration Successful')</script> ";
+}
+}
+}
 ?>
 
 <!DOCTYPE html>
